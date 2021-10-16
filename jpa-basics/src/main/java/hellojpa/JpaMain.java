39a838fe492b64ca27f85743991f041e9d993e56
danielpark0 <37523983+danielpark0@users.noreply.github.com>
2021-10-16 23:11:55 +0900

    양방향 매핑

--- jpa-basics/src/main/java/hellojpa/JpaMain.java
+++ jpa-basics/src/main/java/hellojpa/JpaMain.java
@@ -24,8 +24,10 @@ public class JpaMain {
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setTeam(team);
+            //member.changeTeam(team);
             em.persist(member);
+
+            team.addMember(member);
 
             Member findMember = em.find(Member.class, member.getId());
 
