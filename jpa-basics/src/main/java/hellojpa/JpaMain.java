5903e54281313743617b17128b87cc9f42dac2b7
danielpark0 <37523983+danielpark0@users.noreply.github.com>
2021-10-25 19:54:41 +0900

    mapped super class

--- jpa-basics/src/main/java/hellojpa/JpaMain.java
+++ jpa-basics/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class JpaMain {
@@ -17,6 +18,13 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            Member member = new Member();
+            member.setUsername("user1");
+            member.setCreatedBy("kim");
+            member.setCreatedDate(LocalDateTime.now());
+
+            em.persist(member);
 
             Movie movie = new Movie();
             movie.setDirector("aaaa");
