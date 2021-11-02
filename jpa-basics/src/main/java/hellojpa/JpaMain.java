8dbbcc7bf74c78c020c9652b73691f48630598c2
danielpark0 <37523983+danielpark0@users.noreply.github.com>
2021-11-02 21:42:43 +0900

    기본객체, 불변객체

--- jpa-basics/src/main/java/hellojpa/JpaMain.java
+++ jpa-basics/src/main/java/hellojpa/JpaMain.java
@@ -19,12 +19,21 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("hello");
-            member.setHomeAddress(new Address("city", "street", "10000"));
-            member.setWorkPeriod(new Period());
+            Address address = new Address("city", "street", "10000");
 
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setHomeAddress(address);
             em.persist(member);
+
+            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+
+            Member member2 = new Member();
+            member.setUsername("member2");
+            member.setHomeAddress(address);
+            em.persist(member2);
+
+            member.getHomeAddress().setCity("newCity");
 
             tx.commit();
         } catch (Exception e) {
